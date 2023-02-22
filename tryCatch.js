2b7e2db23a37dc7f10c3c07458a5e35c6644cef1
vatiza <rakibur.mac@gmail.com>
2023-02-23 02:55:23 +0600

    date.js file add

---
+++ tryCatch.js
@@ -0,0 +1,16 @@
+const day = 'day the din';
+console.log(day);
+console.log(25);
+// console.log(district);
+// let district = 'Bbari';
+// console.log(61);
+try {
+    console.log(666);
+    console.log(stdl);
+}
+catch (error) {
+    console.log("not found");
+}
+finally {
+    console.log('finally done');
+}
