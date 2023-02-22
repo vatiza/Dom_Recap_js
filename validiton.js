0fc2ced828f194eb440f6af2f19d9274a57c48fa
vatiza <rakibur.mac@gmail.com>
2023-02-23 03:16:50 +0600

    validiton.js file add

---
+++ validiton.js
@@ -0,0 +1,14 @@
+const number = 23;
+
+if (typeof number === 'number') {
+    console.log('value is a number');
+}
+else {
+    console.log('value is not a number');
+}
+const numbers = [3, 23, 34, 45];
+const student = {
+    name:'rakin', old:17,dept:'cse'
+}
+console.log(Array.isArray(number));
+console.log(typeof student);
