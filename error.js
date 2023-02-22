68c35e9eb11bf7e482f17891536436f7005fbff9
vatiza <rakibur.mac@gmail.com>
2023-02-23 02:43:58 +0600

    try catch file

---
+++ error.js
@@ -0,0 +1,12 @@
+const numbers = [3, 5, 4, 6, 7];
+
+numbers.length = 2;
+console.log(numbers);
+console.log(numbers.length);
+// console.log(numbers[5]);
+
+// console.log(money);
+const money = 80;
+for (let i = 0; i < 5; i++){
+    
+}
